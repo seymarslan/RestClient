bacc81d4e0c1df81f503555c6bf90576e1c5733e
seymarslan <inreallifeseyma@gmail.com>
2022-01-26 17:03:44 +0300

    Restclient basic example

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace RestclientBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            RestClient client = new RestClient("https://jsonplaceholder.typicode.com/todos");
+
+            RestRequest request = new RestRequest(Method.GET);
+            var content = client.Execute(request);
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(content.Content).ToList();
+            foreach (var user in users)
+            {
+                Console.WriteLine("-----------------------------------------------");
+                //Console.WriteLine("UserId:" + user.userId);
+                //Console.WriteLine("Id:" + user.id);
+                
+                Console.WriteLine("Title:" + user.title);
+                //Console.WriteLine("Completed:" + user.completed);
+                users.Any(x => x.completed == true);
+                
+            }
+            
+           
+
+            Console.ReadLine();
+           
+        }
+        
+
+    }
+
+
+}
---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestclientBasic
+{
+    
+    public class User
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+
+        internal object Select(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object Where(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object Any(Func<object, object> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
